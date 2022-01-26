94f96cf3fdb94547de544576a5797207492838cb
rashedul24 <ronyuap@gmail.com>
2022-01-26 21:52:52 +0600

    private route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import AuthProvider from './context/AuthProvider';
+import DashBoard from './pages/DashBoard/DashBoard';
 import Home from './pages/Home/Home/Home';
+import PrivateRoute from './pages/Login/Firebase/PrivateRoute/PrivateRoute';
 import Login from './pages/Login/Login';
 import Products from './pages/Products/Products';
 
@@ -16,9 +18,11 @@ function App() {
       </Route>
       <Route path="/home" element={<Home />}>
       </Route>
-      <Route path="/products" element={<Products />}>
+      <Route path="/products" element={<PrivateRoute><Products /></PrivateRoute>}>
       </Route>
       <Route path="/login" element={<Login />}>
+      </Route>
+      <Route path="/dashboard" element={<PrivateRoute><DashBoard /></PrivateRoute>}>
       </Route>
       
     </Routes>
--- src/pages/Login/Firebase/PrivateRoute/PrivateRoute.js
+++ src/pages/Login/Firebase/PrivateRoute/PrivateRoute.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
-const PrivateRoute = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { Spinner } from 'react-bootstrap';
+import { Navigate, useLocation,} from 'react-router-dom';
+import useAuth from '../../../../hooks/useAuth';
+const PrivateRoute = ({ children }) => {
+  const location = useLocation();
+  const { user, isLoading } = useAuth();
+  if (isLoading) {
+    return <div className='text-center m-5'>
+      <Spinner animation="border" variant="primary"></Spinner>
+    </div> 
+  }
+  return (
+    
+      user.displayName ? children :
+          <Navigate
+            to= "/login" replace state={{from: location}}
+          ></Navigate>
+    
+  );
 };
 
 export default PrivateRoute;
