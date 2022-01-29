6de657c676794966a52567d81bfc7eea02862f69
rashedul24 <ronyuap@gmail.com>
2022-01-29 21:16:10 +0600

    admin added

--- src/pages/DashBoard/AddAdmin/AddAdmin.js
+++ src/pages/DashBoard/AddAdmin/AddAdmin.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 const AddAdmin = () => {
+    const [email, setEmail]=useState('');
+    const handleOnBlur= e =>{
+        setEmail(e.target.value);
+    }
+    const handleAddAdmin= e =>{
+        const user = {email};
+        fetch('http://localhost:5000/users/admin', {
+            method : 'PUT',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+        .then(res =>res.json())
+        .then(data=>{
+            console.log(data);
+            if (data.modifiedCount) {
+                alert("Admin Added Successfully");
+            }
+        })
+
+        e.preventDefault()
+    }
     return (
-        <div>
-             <h2>add admin</h2>
+        <div className='text-center'>
+             <h1 className="mb-5 text-primary fst-italic">Add an Admin</h1>
+             <form onSubmit={handleAddAdmin}>
+             <TextField  
+             sx={{width : '50%'}}
+             label="email" 
+             type="email" 
+             variant="standard" 
+             onBlur={handleOnBlur}
+             />
+             <Button type='submit' variant='contained'>Add Admin</Button>
+             </form>
         </div>
     );
 };
