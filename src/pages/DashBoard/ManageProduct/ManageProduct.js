4d8ce85c26b93c076685a26a787aa353774d5338
rashedul24 <ronyuap@gmail.com>
2022-01-29 20:20:05 +0600

    manage product added

--- src/pages/DashBoard/ManageProduct/ManageProduct.js
+++ src/pages/DashBoard/ManageProduct/ManageProduct.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import { Card } from "@mui/material";
+import React, { useEffect, useState } from "react";
 
 const ManageProduct = () => {
-    return (
-        <div>
-            <h2>manage product</h2>
-        </div>
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/allProducts")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  const handleDelete = (id) => {
+    const permit = window.confirm(
+      "Are you sure? Deleted data can not retrieve."
     );
+    if (permit) {
+      const url = `http://localhost:5000/allProducts/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount) {
+            alert("Deleted Successfully");
+            const rest = products.filter((product) => product._id !== id);
+            setProducts(rest);
+          }
+        });
+    }
+  };
+  return (
+    <div className="text-center">
+      <h1 className="mb-5 text-primary fst-italic">Manage Products</h1>
+     <div className="row">
+     {products.map((product) => (
+        <div key={product._id}
+        className='col-md-4 col-sm-12'
+        >
+          <Card className="text-center p-5 m-3 shadow ">
+          <img className='w-75 ms-4 mb-3' src={product.img} alt="" />
+          <h3><span className="fw-bold">Product</span>: {product.name}</h3>
+          <p><span className="fw-bold fs-4">Description</span>: {product.description}</p>
+          <h3>Price: $ {product.price}</h3>
+          <button
+            onClick={() => handleDelete(product._id)}
+            className="btn btn-danger px-3 m-3"
+          >
+            Delete
+          </button>
+          </Card>
+        </div>
+      ))}
+     </div>
+    </div>
+  );
 };
 
 export default ManageProduct;
